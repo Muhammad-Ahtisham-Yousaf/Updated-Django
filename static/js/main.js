8020550a8eb273ceeb90cc3cf7674d17f067a069
Muhammad-Ahtisham-Yousaf <157544539+Muhammad-Ahtisham-Yousaf@users.noreply.github.com>
2024-03-08 09:48:57 +0500

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -11,25 +11,24 @@ function hideSidebar(){
 // swiper
 // <!-- Initialize Swiper -->
 var swiper = new Swiper(".mySwiper", {
-        effect: "card",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        speed:1000,
-        modifier: 1,
-        slideShadows: true,
-     },
-        pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+  rotate: 50,
+  stretch: 0,
+  depth: 100,
+  modifier: 1,
+  slideShadows: true,
+},
+  pagination: {
+el: ".swiper-pagination",
+clickable: true,
+},
+
   
-        
-  });
+});
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     const filterSelects = document.querySelectorAll('.filter-select');
